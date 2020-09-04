4b9a46c9cbee65fe3008b2dc890ffc2595c7cdc6
hellosurbhi <messagesurbhi@gmail.com>
2020-09-04 09:53:28 -0700

    #00 final

--- 00.js
+++ 00.js
@@ -1,5 +1,13 @@
-// This is the solution for the Codecademy challenge #00 available on https://discuss.codecademy.com/t/essential-information-on-code-challenges/83909
+// This is the solution for the Codecademy challenge #00 
 
+// "Write a program that prints the numbers from 1 to 100. 
+// But for multiples of three print Fizz instead of the number 
+// and for the multiples of five print Buzz. 
+// For numbers which are multiples of both three and five print FizzBuzz."
+
+//Initially I added the forloop in a function with a number variable 
+// and called the function with 100 as the param but the function isn't necessary
+// to run the forloop, especially when we have a single param to work with. 
 
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
